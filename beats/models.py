af277a2a97eeb48679f25cf0753baa2b93264c81
cobobc <ciaranob4@gmail.com>
2022-01-24 22:38:13 +0000

    Upload audio files and load genre data

--- beats/models.py
+++ beats/models.py
@@ -14,11 +14,10 @@ class Genre(models.Model):
 
 class Beat(models.Model):
     genre = models.ForeignKey('Genre', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    file = models.FileField(upload_to='media/audio/')
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
