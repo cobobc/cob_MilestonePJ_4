f5f2e1883148c27f1ba1228fcdc5196b1bf7afca
cobobc <ciaranob4@gmail.com>
2022-01-25 21:55:10 +0000

    customize admin

--- beats/admin.py
+++ beats/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Beat, Genre
 
 # Register your models here.
-admin.site.register(Beat)
-admin.site.register(Genre)
+
+class BeatAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'genre',
+        'price',
+        'file',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Beat, BeatAdmin)
+admin.site.register(Genre, GenreAdmin)
