d2c8c8a5215c39e6b2dca6a52eb1cc20b1b9d57f
cobobc <ciaranob4@gmail.com>
2022-01-25 22:19:25 +0000

    Add product views and templates

--- beats/views.py
+++ beats/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Beat
 
 # Create your views here.
+
+def all_beats(request):
+    """ A view to show all beats, including sorting and search queries """
+
+    beats = Beat.objects.all()
+
+    context = {
+        'beats': beats,
+    }
+
+    return render(request, 'beats/beats.html', context)
