5d705a53391455f7eb2f4b13f8e8ad3f61d81214
cobobc <ciaranob4@gmail.com>
2022-01-27 21:55:34 +0000

    Add search functionality

--- beats/views.py
+++ beats/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Beat
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_beats(request):
     """ A view to show all beats, including sorting and search queries """
 
     beats = Beat.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('beats'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            beats = beats.filter(queries)
 
     context = {
         'beats': beats,
+        'search_term': query,
     }
 
     return render(request, 'beats/beats.html', context)
