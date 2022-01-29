44ca31b9696ed686aad9f729b0de820677dc7497
cobobc <ciaranob4@gmail.com>
2022-01-29 21:57:23 +0000

    update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,23 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from beats.models import Beat
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     beat_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        beat = get_object_or_404(Beat, pk=item_id)
+        total += quantity * beat.price
+        beat_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'beat': beat,
+        })
 
     grand_total = total
 
