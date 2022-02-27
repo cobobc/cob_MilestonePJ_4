0ce951892736df68104e40adb1f24362060e20db
cobobc <ciaranob4@gmail.com>
2022-02-27 23:10:52 +0000

    Removed secret key from settings.py

--- fob_beats/settings.py
+++ fob_beats/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-4*%_g4vvr2u2%by#yd5^*a9lcktc$50u1c*ck_e+ljd_+n=t*5')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
